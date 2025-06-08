c3882650f97aee024cd9e5a632be1eba60f5938b
jamartule <marta.zaldokaite@gmail.com>
2025-06-08 16:55:58 +0300

    Pridėtas papildytas

--- frontend/src/components/EquipmentList.js
+++ frontend/src/components/EquipmentList.js
@@ -10,15 +10,32 @@ function EquipmentList() {
   }, []);
 
   return (
-    <div>
-      <h2>Įrangos sąrašas</h2>
-      <ul>
+    <div className="container">
+      <h2 className="mt-4 mb-4">Įrangos sąrašas</h2>
+      <div className="row">
         {equipment.map((item) => (
-          <li key={item.id}>
-            <Link to={`/equipment/${item.id}`}>{item.name}</Link>
-          </li>
+          <div className="col-md-4 mb-4" key={item.id}>
+            <div className="card h-100 shadow-sm">
+              {item.image && (
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="card-img-top"
+                  style={{ maxHeight: '200px', objectFit: 'cover' }}
+                />
+              )}
+              <div className="card-body">
+                <h5 className="card-title">
+                  <Link to={`/equipment/${item.id}`}>{item.name}</Link>
+                </h5>
+                {item.description && (
+                  <p className="card-text">{item.description}</p>
+                )}
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
